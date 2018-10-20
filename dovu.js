7118a85aed5ec0a5360b4e0a48ea2b5a5fbdcea5
Max Silva <maxsilvauk@gmail.com>
2018-10-20 23:24:20 +0100

    Tidy

--- dovu.js
+++ dovu.js
@@ -1,51 +1,35 @@
 var journeys = {};
 
-
 function getJourneys() {
+  fetch('https://play.bristolfilmacademy.com/journey')
+      .then((response) => response.json())
+      .then((responseJson) => {
+          journeys = responseJson;
 
-
-    fetch('https://play.bristolfilmacademy.com/journey')
-        .then((response) => response.json())
-        .then((responseJson) => {
-            journeys = responseJson;
-            //journeys.forEach(mapJourneys);
-
-            console.log(journeys);
-
-            for (i=0;i<journeys.length;i++) {
-
-              // create a closure to preserve the value of "i"
-              (function(i){
-                window.setTimeout(function(){
-                    console.log(i);
-                  mapJourneys(journeys[i]);
-                }, i * 500);
-
-              }(i));
-
-            }
-        })
-        .catch(function(error) {
-          console.log('There has been a problem with your fetch operation: ' + error.message);
-           // ADD THIS THROW error
-          throw error;
-        });
+          for (i=0;i<journeys.length;i++) {
+            (function(i){
+              window.setTimeout(function(){
+                console.log(i);
+                mapJourneys(journeys[i]);
+              }, i * 500);
+            }(i));
+          }
+      })
+      .catch(function(error) {
+        console.log('There has been a problem with your fetch operation: ' + error.message);
+        throw error;
+      });
 }
 
 function mapJourneys(journey) {
-
-
-
-    var redLine = viewer.entities.add({
-        name : 'Red line on the surface',
-        polyline : {
-            positions : Cesium.Cartesian3.fromDegreesArray([journey.start_lat, journey.start_long,
-                                                            journey.end_lat, journey.end_long]),
-            width : 1,
-            material : Cesium.Color.TURQUOISE
-        }
-    });
+  var redLine = viewer.entities.add({
+      name : 'Red line on the surface',
+      polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([journey.start_lat, journey.start_long, journey.end_lat, journey.end_long]),
+        width : 1,
+        material : Cesium.Color.TURQUOISE
+      }
+  });
 }
-
 
 getJourneys();
